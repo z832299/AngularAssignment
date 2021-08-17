4d7ffaa30246d8a3c6eecbe056107330e0c8714c
unknown <nishanthi.deva@gmail.com>
2021-08-17 12:00:49 +0530

    Final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { Component, ViewChild } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {delay} from 'rxjs/operators';
+import {MatSidenav} from '@angular/material/sidenav';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,65 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myDashboard';
+  options: FormGroup;
+  
+  searchedKeyword: string = '';
+  @ViewChild(MatSidenav) sidenav! : MatSidenav;
+  membersArray :{ title: string; subtitle: string; content: string; }[] = [];
+  constructor(fb: FormBuilder,private observer: BreakpointObserver) {
+    this.options = fb.group({
+      bottom: 0,
+      fixed: false,
+      top: 0
+    });
+    
+  }
+  ngAfterViewInit(){
+    this.observer.observe(['(max-width: 720px)']).pipe(delay(1)).subscribe((res) =>{
+      if(res.matches){
+        this.sidenav.mode = 'over';
+        this.sidenav.close();
+       
+      }else{
+        this.sidenav.mode = 'side';
+        this.sidenav.open();
+        
+      }
+    });
+  }
+
+
+  menus = [
+        {id:1, name: 'All'},
+        {id:2, name: 'Finance'},
+        {id:3, name: 'Technical'},
+        {id:4, name: 'Marketing'},
+        {id:5, name: 'Human Resource'}
+
+        ];
+
+  members: {title: string, subtitle: string, content: string}[] = [
+      {title: 'Technical', subtitle: 'Subtitle', content: 'Terminal instructables' },
+      {title: 'Finance', subtitle: 'Subtitle', content: 'Customer Onboarding'},
+      {title: 'Human Resource', subtitle: 'Subtitle', content: 'Career oppertunities'},
+      {title: 'Technical', subtitle: 'Subtitle', content: 'installation Guide'},
+      {title: 'Marketing', subtitle: 'Subtitle', content: 'Printing Guidence'},
+      {title: 'Human Resource', subtitle: 'Subtitle', content: 'Training'},
+      {title: 'Marketing', subtitle: 'Subtitle', content: 'Oulet Branding'},
+      {title: 'Technical', subtitle: 'Subtitle', content: 'Technical Support'},
+      {title: 'Finance', subtitle: 'Subtitle', content: 'Accounting Proceedures'}     
+    ];
+
+    public showInfo(link:string) {
+      
+      if(link !== "All"){
+       this.searchedKeyword = link;
+       
+      }
+      else {
+      this.searchedKeyword = "";
+      }
+    }
+     
+    
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MaterialModule } from './material-module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { Ng2SearchPipeModule  } from 'ng2-search-filter';
+import { FlexLayoutModule } from '@angular/flex-layout';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    Ng2SearchPipeModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
